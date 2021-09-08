bf7d972b86f51a9ad0c9262b127325f04b93609b
Jacob Concepcion <jacobdag@ad.unc.edu>
2021-09-08 19:19:47 -0400

    New

---
+++ lessons/if-then-ex.py
@@ -0,0 +1,15 @@
+"""Practice."""
+
+
+choice: int = int(input("Enter a number: "))
+
+if choice < 25:
+    print("A")
+else: 
+    if choice < 50: 
+        print("B")
+    else:
+        if choice <= 75:
+            print("D")
+        else:
+            print("C")
