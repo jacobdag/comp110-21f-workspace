663b2cc3f281f56a46794264a060b4e389ec85b6
Jacob Concepcion <jacobdag@ad.unc.edu>
2021-09-08 21:48:35 -0400

    Fixed

--- exercises/ex02/repeat_beat.py
+++ exercises/ex02/repeat_beat.py
@@ -2,17 +2,17 @@
 
 __author__ = "730386191"
 
-
 beat: str = input("What beat do you want to repeat? ")
 repeat: int = int(input("How many times do you want me to repeat it? "))
 i: int = 0
 repeat_two: int = repeat
-beat_multi: str = (str(beat + " ") * int(repeat))
+space: str = (" ")
+beat_multi: str = str(" " + beat) * int(repeat - 1)
 
 while i >= repeat:
     print("No beat...")
     repeat = i + 1
 
 while i < repeat_two:
-    print(beat_multi)
+    print(beat + beat_multi)
     repeat_two = i
