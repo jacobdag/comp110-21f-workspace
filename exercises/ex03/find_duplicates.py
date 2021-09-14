65470ba009c6c8ffd2dbf2a564dea66e71472bd5
Jacob Concepcion <jacobdag@ad.unc.edu>
2021-09-14 19:29:24 -0400

    Done

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,17 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730386191"
+
+word: str = input("Enter a word: ")
+i: int = 0
+dup: bool = False
+
+while i < len(word):
+    char: str = word[i]
+    j: int = i + 1
+    while j < len(word):
+        if word[j] == char:
+            dup = True
+        j = j + 1
+    i = i + 1
+print(dup)
--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -1,6 +1,16 @@
 """Drawing forests in a loop."""
 
-__author__ = "123456789"
+__author__ = "730386191"
 
 # The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
+depth: int = int(input("Depth: "))
+depth_two: int = depth
+i: int = 0
+counter: int = 0
+
+while counter < depth_two:
+    print(TREE * int((depth - depth_two) + 1))
+    depth = depth + 1
+    counter = counter + 1
+print()
