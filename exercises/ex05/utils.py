3ad6be32b959e92eed3d933e1ca9c5bf118639ec
Jacob Concepcion <jacobdag@ad.unc.edu>
2021-10-05 18:47:30 -0400

    Utils and tests

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -2,4 +2,35 @@
 
 __author__ = "123456789"
 
-# Define your functions below
+
+def only_evens(x: list[int]) -> list[int]:
+    """Forms list of only even numbers."""
+    i: int = 0
+    y: list[int] = []
+    while i < (len(x)):
+        if (x[i] % 2) == 0:
+            y.append(x[i])
+        i += 1
+    return (y)
+
+
+def sub(a_list: list[int], x: int, y: int) -> list[int]:
+    """Forms subset list of given list."""
+    i: int = 0
+    new_list: list[int] = []
+    while i < (len(a_list)):
+        if x < 0:
+            x = 0
+        if a_list[i] == a_list[x] or ((a_list[i]) < a_list[y] and a_list[i] > a_list[x]):
+            new_list.append(a_list[i])
+        i += 1
+    return (new_list)
+
+
+def concat(a_list: list[int], b_list: list[int]) -> list[int]:
+    new_list: list[int] = []
+    for item in a_list:
+        new_list.append(item)
+    for item in b_list:
+        new_list.append(item)
+    return (new_list)
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -4,3 +4,62 @@
 # from exercises.ex05.utils import only_evens, sub, concat
 
 __author__ = "123456789"
+
+from exercises.ex05.utils import only_evens, sub, concat
+
+
+# Only even tests.
+def test_only_evens() -> None:
+    x: list[int] = [1, 2, 3, 4, 5, 6]
+    assert only_evens(x) == [2, 4, 6]
+
+
+def test_only_evens_random() -> None:
+    x: list[int] = [2, 5, 4, 6, 7, 10, 12, 14]
+    assert only_evens(x) == [2, 4, 6, 10, 12, 14]
+
+
+def test_only_evens_no_input() -> None:
+    x: list[int] = []
+    assert only_evens(x) == []
+
+
+# Sub Tests.
+def test_sub() -> None:
+    a_list: list[int] = [10, 20, 30, 40]
+    x: int = 1
+    y: int = 3
+    assert sub(a_list, x, y) == [20, 30]
+
+
+def test_sub_large() -> None:
+    a_list: list[int] = [10, 11, 12, 13, 14, 15, 16, 17]
+    x: int = 2
+    y: int = 6
+    assert sub(a_list, x, y) == [12, 13, 14, 15]
+
+
+def test_sub_over_under() -> None:
+    a_list: list[int] = [10, 20, 30, 40]
+    x: int = -1
+    y: int = 3
+    assert sub(a_list, x, y) == [10, 20, 30]
+
+
+# Concat tests.
+def test_concat() -> None:
+    a_list: list[int] = [1, 2, 3, 4]
+    b_list: list[int] = [5, 6, 7, 8]
+    assert concat(a_list, b_list) == [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+def test_concat_small() -> None:
+    a_list: list[int] = [1, 2]
+    b_list: list[int] = [4, 7, 8]
+    assert concat(a_list, b_list) == [1, 2, 4, 7, 8]
+
+
+def test_concat_no_input() -> None:
+    a_list: list[int] = [1, 2, 3, 4]
+    b_list: list[int] = []
+    assert concat(a_list, b_list) == [1, 2, 3, 4]
