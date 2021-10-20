ec7911cd20845fa0b0a92fc240e8e2be4293fe32
Jacob Concepcion <jacobdag@ad.unc.edu>
2021-10-19 22:28:23 -0400

    Testing

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,43 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730386191"
 
-# Define your functions below
+
+def invert(set: dict[str, str]) -> dict[str, str]:
+    """Inverts key and value."""
+    inverted_set = dict()
+    for key in set:
+        value = set[key]
+        inverted_set[value] = key
+    return(inverted_set)
+
+
+def favorite_color(colors: dict[str, str]) -> str:
+    """Returns most common color."""
+    i = 0
+    count = 0
+    maximum = 0
+    list_key = list()
+    final_max = ""
+    for key in colors:
+        list_key.append(colors[key])
+    for item in list_key:
+        while i < len(list_key) - 1:
+            if list_key[i] == item:
+                count += 1
+                i += 1
+                if count >= maximum:
+                    maximum = count
+                    final_max = item
+    return(final_max)        
+
+
+def count(list_one: list[str]) -> dict[str, int]:
+    """Count the value."""
+    new_dict = dict()
+    for item in list_one:
+        if item in new_dict:
+            new_dict[item] += 1
+        else: 
+            new_dict[item] = 1
+    return(new_dict)
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,61 @@
 """Unit tests for dictionary functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
 
-__author__ = "123456789"
+from exercises.ex06.dictionaries import invert, favorite_color, count
+
+
+__author__ = "730386191"
+
+
+def test_invert() -> None:
+    """Standard use invert dictionary characters."""
+    set: dict = {'a': 'z', 'b': 'y', 'c': 'x'}
+    assert invert(set) == {'z': 'a', 'y': 'b', 'x': 'c'}
+
+
+def test_invert_words() -> None:
+    """Standard use invert dictionary characters."""
+    set: dict = {'Monster': 'Cookie', 'Dog': 'Cat', 'Then': 'Ok'}
+    assert invert(set) == {'Cookie': 'Monster', 'Cat': 'Dog', 'Ok': 'Then'}
+
+
+def test_invert_none() -> None:
+    """No input."""
+    set: dict = {'': ''}
+    assert invert(set) == {'': ''}
+
+
+def test_favorite_color_small() -> None:
+    """Standard use small sample."""
+    colors: dict = {"Marc": "yellow", "Ezri": "blue", "Kris": "blue"}
+    assert favorite_color(colors) == "blue"
+
+
+def test_favorite_color_large() -> None:
+    """Standard use large sample."""
+    colors: dict = {"J": "Purple", "A": "Green", "K": "Teal", "M": "Teal", "F": "Purple", "G": "Teal"}
+    assert favorite_color(colors) == "Purple"
+
+
+def test_favorite_color_tie() -> None:
+    """Tie Scenario."""
+    colors: dict = {"J": "Purple", "K": "Teal", "M": "Teal", "F": "Purple"}
+    assert favorite_color(colors) == "Purple"
+
+
+def test_no_repeats() -> None:
+    """No repeats."""
+    list_one: list = ["j", "a", "k", "e"]
+    assert count(list_one) == {"j": 1, "a": 1, "k": 1, "e": 1}
+
+
+def test_repeats() -> None:
+    """Repeats."""
+    list_one: list = ["j", "j", "a", "a"]
+    assert count(list_one) == {"j": 2, "a": 2}
+
+
+def test_repeat_once() -> None:
+    """One repeat."""
+    list_one: list = ["j", "j", "a", "k", "e"]
+    assert count(list_one) == {"j": 2, "a": 1, "k": 1, "e": 1}
